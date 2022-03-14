678bbe9e72d15d449cfbce39c9e36456defcc972
Qais Khan <84249944+Qais-Khan@users.noreply.github.com>
2022-03-14 02:21:08 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,16 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="*")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
-@bot.command()
+@bot.command(name="suby")
 async def ping(ctx):
-    await ctx.send("pong")
+    await ctx.send("sucks")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
